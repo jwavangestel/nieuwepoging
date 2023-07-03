bfc624f71726f87dd176d626b6c6dcaed1cd2d38
jwavangestel <jwa.van.gestel@gmail.com>
2023-07-03 22:06:17 +0200

    dbJson toegevoegd

--- src/utils.js
+++ src/utils.js
@@ -2,8 +2,8 @@ export function uuid () {
   return Math.random().toString(16).slice(2)
 }
 
-export function saveStatePlugin (store) {
-  store.subscribe(
+export function saveStatePlugin (todoList) {
+  todoList.subscribe(
     (mutation, state) => localStorage.setItem('boards', JSON.stringify(state.boards))
   )
 }
